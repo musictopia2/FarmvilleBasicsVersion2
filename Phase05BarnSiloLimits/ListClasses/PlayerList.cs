ef53b806fc25cd950da919523d9a296ea64e6b17
Andy Walter <musictopia2@hotmail.com>
2026-01-07 15:27:03 -0700

    Removed future phase from here since it belongs to next phase.

--- Phase05BarnSiloLimits/ListClasses/PlayerList.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Phase05BarnSiloLimits.ListClasses;
-public static class PlayerList
-{
-    public const string Andy = "Andy";
-    public const string Cristina = "Cristina";
-}
--- Phase05BarnSiloLimits/Models/EnumWorksiteState.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Phase05BarnSiloLimits.Models;
-public enum EnumWorksiteState
-{
-    None,
-    Processing,
-    Collecting
-}

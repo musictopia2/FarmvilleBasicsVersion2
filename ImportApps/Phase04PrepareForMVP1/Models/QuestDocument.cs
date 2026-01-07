0fa07775afc022b72ec7846ccdc487a4b313484a
Andy Walter <musictopia2@hotmail.com>
2026-01-07 16:46:00 -0700

    Removed the previous phase work so this only focused on the work completed in this phase.

--- ImportApps/Phase04PrepareForMVP1/Models/QuestDocument.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Phase04PrepareForMVP1.Models;
-public class QuestDocument
-{
-    required public FarmKey Farm { get; set; }
-    required public BasicList<QuestRecipe> Quests { get; set; }
-}
--- WebApps/Phase01IntroduceBarnAndSilo/Services/Workshops/IWorkshopPersistence.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Phase01IntroduceBarnAndSilo.Services.Workshops;
-public interface IWorkshopPersistence
-{
-    Task SaveWorkshopsAsync(BasicList<WorkshopAutoResumeModel> workshops);
-}
